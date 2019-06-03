a2b8e34708a36008d6875e29fab9c474d85c5109
Pratyush Khare <khare.pratyush@outlook.com>
2019-06-03 15:40:01 +0530

    Init

---
+++ 02. src/01. Bootstrapping/init.py
@@ -0,0 +1,37 @@
+import warnings
+warnings.filterwarnings('ignore')
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import pandas_profiling
+
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+import xgboost
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.preprocessing import LabelEncoder
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('dark')
+%matplotlib notebook
+from boruta import BorutaPy
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+from scipy.stats import skew
+from scipy.special import boxcox1p
+
+np.random.seed(940702)
